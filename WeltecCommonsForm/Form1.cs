5422bd0e78554e10cbaa4dade1c70e8e7cbff564
Comonsents <hodgsonbr21@gmail.com>
2023-05-31 22:37:48 +1200

    CSV file read and displayed in list box

--- WeltecCommonsForm/Form1.cs
+++ WeltecCommonsForm/Form1.cs
@@ -2,10 +2,48 @@ namespace WeltecCommonsForm
 {
     public partial class FormMain : Form
     {
+        
+        SortedDictionary<string, Catalogue> itemInfo = new SortedDictionary<string, Catalogue>();
+        static string[] headers = new string[6];
+        Catalogue catalogue = new Catalogue();
         public FormMain()
         {
-            InitializeComponent();
 
+            
+            InitializeComponent();
+            var path = @"../../../datafiles/records.csv";
+            string[] lines = File.ReadAllLines(path);
+
+            foreach (string line in lines) 
+            {
+                if (line.StartsWith("Type"))
+                {
+                    headers = line.Split(' ');
+                }
+                else
+                {
+                    string[] info = line.Split(',');
+                    itemInfo.Add(info[3], new Catalogue //Title, Author, Description, ISBN, Type, Year
+                    {
+                        Author = info[2],
+                        Description = info[5],
+                        Isbn = info[1],
+                        Type = info[0],
+                        Year = int.Parse(info[4])
+                        
+                    });
+                }
+            }
+
+            RefreshForm();
+
+        }
+
+        private void RefreshForm()
+        {
+            CurrentBorrowed.Sorted = true;
+            CurrentBorrowed.DataSource = new BindingSource(itemInfo, null);
+            CurrentBorrowed.ValueMember = "Key";
         }
 
         private void CurrentBorrowed_SelectedIndexChanged(object sender, EventArgs e)
